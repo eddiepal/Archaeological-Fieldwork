47a8c86d856bfd62da47fa22bf96d426591c14c2
Edward Burke <empburke@outlook.com>
2019-11-03 22:58:57 +0000

    Add simple working with sign out symbol beside it (that is currently unfunctional)

--- app/src/main/java/com/example/archaeological_fieldwork/activities/HillfortListActivity.kt
+++ app/src/main/java/com/example/archaeological_fieldwork/activities/HillfortListActivity.kt
@@ -14,38 +14,57 @@ import com.example.archaeological_fieldwork.models.HillfortModel
 
 class HillfortListActivity : AppCompatActivity(), HillfortListener {
 
-  lateinit var app: MainApp
+    lateinit var app: MainApp
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_hillfort_list)
-    app = application as MainApp
-    toolbar.title = title
-    setSupportActionBar(toolbar)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort_list)
+        app = application as MainApp
+        toolbar.title = title
+        setSupportActionBar(toolbar)
 
-    val layoutManager = LinearLayoutManager(this)
-    recyclerView.layoutManager = layoutManager
-    recyclerView.adapter = HillfortAdapter(app.hillforts.findAll(), this)
-  }
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = HillfortAdapter(app.hillforts.findAll(), this)
+    }
 
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.menu_main, menu)
-    return super.onCreateOptionsMenu(menu)
-  }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+/*  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
     when (item?.itemId) {
       R.id.item_add -> startActivityForResult<HillfortActivity>(0)
     }
     return super.onOptionsItemSelected(item)
-  }
+  }*/
 
-  override fun onHillfortClick(hillfort: HillfortModel) {
-    startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
-  }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.item_add ->  {
+            startActivityForResult<HillfortActivity>(0)
+            // User chose the "Print" item
+            //Toast.makeText(this,"Print action",Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_sign_out ->{
+           // Toast.makeText(this,"Home action",Toast.LENGTH_LONG).show()
+            true
+        }
 
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    recyclerView.adapter?.notifyDataSetChanged()
-    super.onActivityResult(requestCode, resultCode, data)
-  }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onHillfortClick(hillfort: HillfortModel) {
+        startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
